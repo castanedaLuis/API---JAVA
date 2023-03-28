6abdf2ce9aff914acdbd0754e220f102828b7878
castanedaLuis <castanedaLuis@github.com>
2023-03-27 21:09:40 -0600

    Agregando Query Methods 🎖

--- src/main/java/com/jlcastaneda/market/persintence/crud/ProductoCrudRepository.java
+++ src/main/java/com/jlcastaneda/market/persintence/crud/ProductoCrudRepository.java
@@ -3,7 +3,13 @@ package com.jlcastaneda.market.persintence.crud;
 import com.jlcastaneda.market.persintence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto,Integer> {
 
+     List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+     Optional<Producto> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 
 }
