5b48ff5be5003f8292bdba5d109f59b5251f43d1
castanedaLuis <castanedaLuis@github.com>
2023-03-28 00:40:44 -0600

    Implementar la anotación @Repository

--- src/main/java/com/jlcastaneda/market/persintence/ProductoRepository.java
+++ src/main/java/com/jlcastaneda/market/persintence/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.jlcastaneda.market.persintence;
 
 import com.jlcastaneda.market.persintence.crud.ProductoCrudRepository;
 import com.jlcastaneda.market.persintence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository  //le decimos a spring que esta clase interactua con la DB
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -17,7 +19,19 @@ public class ProductoRepository {
         return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
     }
 
-    public Optional<List<Producto>> getEscasos(int cantidad){
-        return  productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true):
+    public Optional<Producto> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public  Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 }
