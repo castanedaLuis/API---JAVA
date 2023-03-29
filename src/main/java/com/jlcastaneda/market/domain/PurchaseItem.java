63754784c2e61e6d8de1471719cc7ad48b23da8f
castanedaLuis <castanedaLuis@github.com>
2023-03-29 14:04:46 -0600

    Crear el dominio de compras 🛒

---
+++ src/main/java/com/jlcastaneda/market/domain/PurchaseItem.java
@@ -0,0 +1,41 @@
+package com.jlcastaneda.market.domain;
+
+public class PurchaseItem {
+
+    private int productId;
+    private int quantity;
+    private long total;
+    private boolean active;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public long getTotal() {
+        return total;
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
---
+++ src/main/java/com/jlcastaneda/market/domain/repository/PurchaseRepository.java
@@ -0,0 +1,13 @@
+package com.jlcastaneda.market.domain.repository;
+
+import com.jlcastaneda.market.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    List<Purchase> getAll();
+    Optional <List<Purchase>> getByClient(int clientId);
+    Purchase save(Purchase purchase);
+
+}
